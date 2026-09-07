f96157cae80e97709710055e76d1eb7f758e7d81
Sistema Solucões VerticalParts <gelson.simoes@verticalparts.com.br>
2026-09-07 14:51:16 -0300

    feat: export tutorial player

---
+++ src/player/index.ts
@@ -0,0 +1,3 @@
+export { TutorialPlayer } from './TutorialPlayer.js';
+export type { TutorialPlayerProps } from './TutorialPlayer.js';
+export type { Tutorial, TutorialStep, TutorialTarget, TutorialAction } from '../types.js';
